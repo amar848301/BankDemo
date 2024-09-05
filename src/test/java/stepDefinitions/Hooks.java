7496a36d300029c024b62abc8eb76c730424a1d4
amar <amar.848301@gmail.com>
2024-09-05 22:11:55 +0530

    removed unused imports

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,12 +1,9 @@
 package stepDefinitions;
 
 import java.io.IOException;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import util.Base;
 
 public class Hooks {
